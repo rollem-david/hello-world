827bf6fdefd73056f8a5bad6e41e63364a9fe492
David Mellor <davidmellor@Davids-MBP.fios-router.home>
2018-09-22 11:13:39 -0400

    forgot file

--- src/helloWorld/AwsRDSConnectionTest.java
+++ src/helloWorld/AwsRDSConnectionTest.java
@@ -1,5 +1,32 @@
 package helloWorld;
 
-public class AwsRDSConnectionTest {
+import java.sql.*;
 
+public class AwsRDSConnectionTest {
+	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://mysql-db1.c5gfxvk6ptv0.us-east-2.rds.amazonaws.com/mysql_db1_master";
+	   
+	   static final String USER = "mysql_db1_master";
+	   static final String PASS = "Tennis100";
+
+	   private Connection connection;
+	   
+	   public void getConnection() {
+		   try {
+			   long startTime = System.currentTimeMillis();
+			   Class.forName("com.mysql.jdbc.Driver");
+			   System.out.println("Starting Connecting to database");
+			   
+			   connection = DriverManager.getConnection(DB_URL,USER,PASS);
+			   
+			   System.out.println("Ending Connection to database took: " + (System.currentTimeMillis() - startTime));
+		   }
+		   catch(Exception e) {
+			   e.printStackTrace();
+		   }
+		    
+	   }
+
+	
+	
 }
